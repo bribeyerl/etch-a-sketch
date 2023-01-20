15071cac935951dcca78e7424b8a267cec370fbf
bribeyerl <briabeyerl@gmail.com>
2023-01-20 14:28:58 -0500

    Write addGrid function

---
+++ main.js
@@ -0,0 +1,9 @@
+function addGrid(x) {
+    for (let rows = 0; rows < x; rows++) {
+        for (let columns = 0; columns < x; columns++) {
+            $('.container').append('<div class="grid"></div>')
+        }
+    }
+    $('.grid').width(960/x)
+    $('.grid').height(960/x)
+}
